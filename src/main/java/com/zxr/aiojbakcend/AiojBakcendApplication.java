50c3c92719808bbc2826083c8bc519fe25b1067d
zxr <2413512693@qq.com>
2024-07-12 22:45:04 +0800

    初始化项目

---
+++ src/main/java/com/zxr/aiojbakcend/AiojBakcendApplication.java
@@ -0,0 +1,13 @@
+package com.zxr.aiojbakcend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiojBakcendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiojBakcendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxr/aiojbakcend/AiojBakcendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxr.aiojbakcend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiojBakcendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
